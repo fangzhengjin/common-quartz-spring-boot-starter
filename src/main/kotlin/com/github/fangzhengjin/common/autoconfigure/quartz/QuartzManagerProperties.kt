663d9a0b8d1a9a6eaab8887d1456a535d791d0b4
fangzhengjin <fangzhengjin@gmail.com>
2019-02-20 23:25:17 +0800

    任务执行器扫描默认扫描Application入口下的包

--- src/main/kotlin/com/github/fangzhengjin/common/autoconfigure/quartz/QuartzManagerProperties.kt
+++ src/main/kotlin/com/github/fangzhengjin/common/autoconfigure/quartz/QuartzManagerProperties.kt
@@ -1,9 +1,6 @@
 package com.github.fangzhengjin.common.autoconfigure.quartz
 
-import org.reflections.Reflections
-import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.util.StringUtils
 
 /**
  * @version V1.0
@@ -15,25 +12,8 @@ import org.springframework.util.StringUtils
  */
 @ConfigurationProperties("customize.common.quartz")
 class QuartzManagerProperties {
-    private companion object {
-        private val basePackage: String =
-            try {
-                val typesAnnotatedWith = Reflections().getTypesAnnotatedWith(SpringBootApplication::class.java)
-                val `package` = typesAnnotatedWith.firstOrNull()?.`package`
-                if (`package` != null) `package`.name else "com.github.fangzhengjin"
-            } catch (e: Exception) {
-                ""
-            }
-    }
-
     var cacheQuartzManagerException: Boolean = true
     var enableController: Boolean = true
     var baseUrl: String = "/task"
-    var scanExecJobPackages: String = ""
-        get() {
-            if (StringUtils.isEmpty(field)) {
-                field = basePackage
-            }
-            return field
-        }
+    var scanExecJobPackages: String = "com.github.fangzhengjin"
 }
