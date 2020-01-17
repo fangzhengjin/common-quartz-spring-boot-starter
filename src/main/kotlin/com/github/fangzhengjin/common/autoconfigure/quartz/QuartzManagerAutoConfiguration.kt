47ec9ac422e5835104c0b53aa4b4883ccdc44198
fangzhengjin <fangzhengjin@gmail.com>
2020-01-17 14:38:01 +0800

    bug fix

--- src/main/kotlin/com/github/fangzhengjin/common/autoconfigure/quartz/QuartzManagerAutoConfiguration.kt
+++ src/main/kotlin/com/github/fangzhengjin/common/autoconfigure/quartz/QuartzManagerAutoConfiguration.kt
@@ -7,16 +7,12 @@ import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.quartz.SchedulerFactoryBean
 import org.springframework.util.StringUtils
-import springfox.documentation.builders.RequestHandlerSelectors
-import springfox.documentation.spi.DocumentationType
-import springfox.documentation.spring.web.plugins.Docket
 
 /**
  * 当项目使用Quartz时，如果Spring容器中不存在QuartzManager则自动创建
@@ -38,39 +34,22 @@ class QuartzManagerAutoConfiguration {
     @ConditionalOnClass(QuartzJobInfo::class)
     @ConditionalOnMissingBean(QuartzManager::class)
     fun createQuartzManager(
-        @Suppress("SpringJavaInjectionPointsAutowiringInspection")
-        schedulerFactory: SchedulerFactoryBean,
-        quartzManagerProperties: QuartzManagerProperties
+            @Suppress("SpringJavaInjectionPointsAutowiringInspection")
+            schedulerFactory: SchedulerFactoryBean,
+            quartzManagerProperties: QuartzManagerProperties
     ): QuartzManager {
         // 如果用户未设置任务执行器扫描目录，自动扫描入口类下的包
         if (StringUtils.isEmpty(quartzManagerProperties.scanExecJobPackages) || quartzManagerProperties.scanExecJobPackages == "com.github.fangzhengjin") {
             quartzManagerProperties.scanExecJobPackages =
-                try {
-                    val typesAnnotatedWith = Reflections().getTypesAnnotatedWith(SpringBootApplication::class.java)
-                    val `package` = typesAnnotatedWith.firstOrNull()?.`package`
-                    if (`package` != null) `package`.name else "com.github.fangzhengjin"
-                } catch (e: Exception) {
-                    "com.github.fangzhengjin"
-                }
+                    try {
+                        val typesAnnotatedWith = Reflections().getTypesAnnotatedWith(SpringBootApplication::class.java)
+                        val `package` = typesAnnotatedWith.firstOrNull()?.`package`
+                        if (`package` != null) `package`.name else "com.github.fangzhengjin"
+                    } catch (e: Exception) {
+                        "com.github.fangzhengjin"
+                    }
         }
         logger.info("QuartzManager jobExec scanBasePackage: ${quartzManagerProperties.scanExecJobPackages}")
         return QuartzManager.init(schedulerFactory, quartzManagerProperties)
-    }
-
-    /**
-     * 注册SwaggerApi
-     */
-    @Bean
-    @ConditionalOnClass(Docket::class)
-    @ConditionalOnProperty(value = ["customize.common.quartz.showInSwagger"], matchIfMissing = false)
-    fun swaggerQuartzApi(): Docket {
-        return Docket(DocumentationType.SWAGGER_2)
-                .groupName("quartz")
-                .useDefaultResponseMessages(false)
-                .forCodeGeneration(true)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.github.fangzhengjin.common.component.quartz.controller"))
-                .apis(RequestHandlerSelectors.any())
-                .build()
     }
 }
---
+++ src/main/kotlin/com/github/fangzhengjin/common/autoconfigure/quartz/SwaggerQuartzApiAutoConfiguration.kt
@@ -0,0 +1,29 @@
+package com.github.fangzhengjin.common.autoconfigure.quartz
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+@ConditionalOnClass(name = ["org.springframework.scheduling.quartz.SchedulerFactoryBean", "springfox.documentation.spring.web.plugins.Docket"])
+class SwaggerQuartzApiAutoConfiguration {
+    /**
+     * 注册SwaggerApi
+     */
+    @Bean
+    @ConditionalOnProperty(value = ["customize.common.quartz.showInSwagger"], matchIfMissing = false)
+    fun swaggerQuartzApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .groupName("quartz")
+                .useDefaultResponseMessages(false)
+                .forCodeGeneration(true)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.github.fangzhengjin.common.component.quartz.controller"))
+                .apis(RequestHandlerSelectors.any())
+                .build()
+    }
+}
